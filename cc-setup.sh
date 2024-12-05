b6ba7e6eca26e2b18866f0e11841765df3452d42
Eric Yan <ericyan676@gmail.com>
2024-12-05 14:42:14 -0500

    Updated compute canada setup script

--- cc-setup.sh
+++ cc-setup.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-#SBATCH --account=def-mlecuyer
-#SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=24G               # memory per node
-#SBATCH --time=0-03:00
-
 # Use this for interactive jobs
-# salloc --time=1:0:0 --account=def-mlecuyer --gpus-per-node=1 --mem=16G
+# salloc --time=0:30:0 --account=def-mlecuyer --gpus-per-node=1 --mem=32G
 
-# Install virtual environment
-module load gcc arrow python/3.10
+# Install python virtual environment
+module load gcc arrow/17.0.0 python/3.10
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
-pip install --no-index -r /home/qfyan/cs848-group-project/cc-req.txt
+pip install --no-index --ignore-installed --no-deps -r /home/qfyan/cs848-group-project/cc-req.txt
 
-# Run test example
-# python ~/cs848-group-project/be_great/examples/test_train.py 
+# Test to make sure that pyarrow is loaded
+python -c "import pyarrow"
+
+cd /home/qfyan/cs848-group-project/be_great/experiments
